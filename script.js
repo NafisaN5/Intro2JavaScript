f64e926461a23c2ef2efcefa69a602e874133155
Nafisa Nawshin <144722350+NafisaN5@users.noreply.github.com>
2024-03-12 23:53:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+document.getElementById("greet").style.color="purple";
+
+}
+function night () {
+  document.body.style.backgroundColor="darkblue"
+}
+function ChangeColor() {
+  document.getElementById("text").style.color="purple" 
+}
